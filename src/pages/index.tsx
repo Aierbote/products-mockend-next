53c19ba573561329538e2e016c16d44625aaa1db
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-29 11:19:23 +0100

    fix: using `getStaticProps`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { title } from "process";
-import {
-	ReactNode,
-	createContext,
-	use,
-	useContext,
-	useEffect,
-	useState,
-} from "react";
+import Image from "next/image";
+import { ReactNode, createContext } from "react";
 
-type TContext = {
-	products: Array<Product> | null;
-	setProducts: (products: Product[]) => void;
-};
+const inter = Inter({ subsets: ["latin"] });
 
 interface Product {
 	qty: number;
@@ -27,70 +14,32 @@ interface Product {
 	image: string;
 	thumbnail: string;
 }
-const inter = Inter({ subsets: ["latin"] });
 
-const AppContext = createContext({
-	products: null,
-	setProducts: (products: Product[]) => {},
-});
-
-interface ContextProps {
-	children: ReactNode;
+export async function getStaticProps() {
+	try {
+		const response = await fetch(
+			"https://mockend.up.railway.app/api/products/"
+		);
+		const data = await response.json();
+		return { props: { products: data } };
+	} catch (error) {
+		console.error("Error fetching products", error);
+		return { props: { products: [] } };
+	}
 }
 
-const ContextProvider = ({ children }: ContextProps) => {
-	const [products, setProducts] = useState<TContext["products"]>(null);
-
-	async function fetchProducts() {
-		try {
-			const result = await fetch(
-				"https://mockend.up.railway.app/api/products/"
-			);
-			const data = await result.json();
-			// TODO : NOTE :
-			const array: Product[] = use(data);
-			setProducts(array);
-			return data;
-		} catch (error) {
-			console.error("Error fetching products", error);
-		}
-	}
-
-	useEffect(() => {
-		fetchProducts();
-	});
-
-	return (
-		<AppContext.Provider value={{ products, setProducts }}>
-			{children}
-		</AppContext.Provider>
-	);
-};
-
-function MyComponent() {
-	const { products } = useContext(AppContext);
-
+export default function Home(products: Array<Product>) {
 	return (
 		<>
-			{products?.map((product: Product) => (
+			<h1>Lets see Products</h1>
+			{products?.map((product) => {
 				<div key={product.id}>
 					<h2>{product.title}</h2>
 					<p>{product.description}</p>
 					<p>{product.price}</p>
-					<img src={product.image} alt={product.title} />
-				</div>
-			))}
-		</>
-	);
-}
-
-export default function Home() {
-	return (
-		<>
-			<ContextProvider>
-				<h1>Lets see Products</h1>
-				<MyComponent />
-			</ContextProvider>
+					<Image src={product.image} alt={product.title} />
+				</div>;
+			})}
 		</>
 	);
 }
